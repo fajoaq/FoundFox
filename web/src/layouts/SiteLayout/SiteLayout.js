379eec943e8dd6b4d847569c78466613142d8d61
Francis J <fajoaq@gmail.com>
2022-06-08 20:46:10 -0400

    init commit

---
+++ web/src/layouts/SiteLayout/SiteLayout.js
@@ -0,0 +1,26 @@
+import styles from './site-layout.module.scss'
+import { Link, routes } from '@redwoodjs/router'
+
+import { CONSTANTS } from 'src/utils/constants'
+
+const SiteLayout = ({ children }) => {
+  return (
+    <>
+      <header className={styles.container}>
+        <Link to={routes.home()}>
+          <h1>{CONSTANTS.SITE_NAME}</h1>
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default SiteLayout
---
+++ web/src/utils/constants.js
@@ -0,0 +1,5 @@
+const CONSTANTS = {
+  SITE_NAME: 'FoundFox',
+}
+
+export { CONSTANTS }
